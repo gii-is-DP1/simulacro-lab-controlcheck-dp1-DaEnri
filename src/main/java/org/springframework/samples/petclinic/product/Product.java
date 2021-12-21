20b0e7e1e72e475e2005499305409c451a23d866
DanielE <danenrdia@alum.us.es>
2021-12-21 11:46:46 +0100

    5 primeros ejercicios realizados

--- src/main/java/org/springframework/samples/petclinic/product/Product.java
+++ src/main/java/org/springframework/samples/petclinic/product/Product.java
@@ -1,13 +1,26 @@
 package org.springframework.samples.petclinic.product;
 
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.Min;
+
+import org.springframework.samples.petclinic.model.NamedEntity;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class Product {
-    Integer id;
-    String name;
+@Entity
+public class Product extends NamedEntity{
+    //Integer id;
+	
+	//@Length(min = 3, max = 50)
+    //String name;
+	
+	@Min(0)
     double price;
+    
+	@ManyToOne
     ProductType productType;
 }
--- src/main/java/org/springframework/samples/petclinic/product/ProductRepository.java
+++ src/main/java/org/springframework/samples/petclinic/product/ProductRepository.java
@@ -1,16 +1,18 @@
 package org.springframework.samples.petclinic.product;
 
 import java.util.List;
-import java.util.Optional;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 
 
-public interface ProductRepository {
+public interface ProductRepository extends CrudRepository<Product, Integer>{
     List<Product> findAll();
-    List<ProductType> findAllProductTypes();
-    Optional<Product> findById(int id);
+    @Query("SELECT pt FROM ProductType pt")
+	List<ProductType> findAllProductTypes();
+//    Optional<Product> findById(int id);
+//    Product save(Product p);
+    
     Product findByName(String name);
-    Product save(Product p);
 }
